5c9a0930754eeb1ae26475a3eb966fd860ddf269
donalkens <akinniyiakinpelumi@gmail.com>
2020-02-01 14:03:44 +0100

    Add project files.

---
+++ DataCenter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataCenter.Startup))]
+namespace DataCenter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
